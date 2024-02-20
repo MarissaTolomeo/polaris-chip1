ade99e2b09484e065915d672c8ea41dc1062372a
MarissaTolomeo <156345123+MarissaTolomeo@users.noreply.github.com>
2024-02-20 13:12:16 -0500

    Make it rain command

--- src/Counter-app.js
+++ src/Counter-app.js
@@ -42,17 +42,48 @@ increase(){
   }
 }
 
+
+updated(changedProperties) {
+  if (changedProperties.has('number') && (this.number === 21)) {
+    return this.makeItRain();// do your testing of the value and make it rain by calling makeItRain
+  }
+}
+
+makeItRain() {
+  // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+  // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+  // will only run AFTER the code is imported and available to us
+  import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
+    (module) => {
+      // This is a minor timing 'hack'. We know the code library above will import prior to this running
+      // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+      // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+      // it's listening for changes so it can react
+      setTimeout(() => {
+        // forcibly set the poppped attribute on something with id confetti
+        // while I've said in general NOT to do this, the confetti container element will reset this
+        // after the animation runs so it's a simple way to generate the effect over and over again
+        this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+      }, 0);
+    }
+  );
+}
+
+
+
+
 decrease(){
   if (this.number > 10) {
   this.number-=1;
   }
 }
 
+
   render() {
     return html`
       <h1>Counter App</h1>
-        <div class='counter-wrapper'>
-          <div class="number">${this.number}</div>
+        <div class='counter-wrapper'> 
+        <confetti-container id="confetti"><h2 class="number">${this.number}</h2></confetti-container>
           <div class="button-wrapper">
             <button id='button1' @click= "${this.decrease}" ?disabled="${this.min === this.number}" >-</button>
             <button id='button2' @click= "${this.increase}" ?disabled="${this.max === this.number}">+</button>
