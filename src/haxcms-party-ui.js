6efd4e4a3413a83ac12483bff07e9f16a45580fa
MarissaTolomeo <156345123+MarissaTolomeo@users.noreply.github.com>
2024-03-20 11:28:46 -0400

    Set up of UI

---
+++ src/haxcms-party-ui.js
@@ -0,0 +1,47 @@
+import { LitElement, html, css } from 'lit';
+
+
+
+export class Alert extends LitElement {
+
+  static get tag() {
+    return 'UI-tag';
+  }
+
+  constructor() {
+    super();
+    
+  }
+
+
+  static get styles() {
+    return css`
+
+
+
+    
+
+  `;
+}
+
+
+render() {
+   
+
+
+    
+    return html`
+        
+    `;
+  }
+    
+
+
+  static get properties() {
+    return { 
+        
+      };
+    }
+}
+
+globalThis.customElements.define(UI.tag, UI);
