5f3b98436daedbec9c9435861c24bb2eb156fc5b
MarissaTolomeo <156345123+MarissaTolomeo@users.noreply.github.com>
2024-04-07 23:53:54 -0400

    Final changes to Project 1

--- src/haxcms-party-ui.js
+++ src/haxcms-party-ui.js
@@ -13,19 +13,14 @@ export class haxcms extends DDD {
       static properties = {
     items: { type: Array },
   }
-
-  
-
   constructor() {
     super();
     this.delete = false;
     this.changed = false;
     this.saved = false;
-    this.party = localStorage.getItem("party") != null ? localStorage.getItem("party").split(",") : ["evp5350", "test1234"];
-    this.selectedUser = "";
-        
+    this.party = localStorage.getItem("party") != null ? localStorage.getItem("party").split(",") : ["mlt5620", "test1234"];
+    this.selectedUser = "";  
   }
-
 
   static get styles() {
     return [ super.styles,css`
@@ -35,7 +30,7 @@ export class haxcms extends DDD {
             font-family: "Press Start 2P", system-ui;
         }
         .List {
-                background-color: var(--ddd-theme-default-beaverBlue);
+                background-color: var(--ddd-theme-default-Blue);
                 min-width: 100vh;
                 height: 620px;
                 padding: var(--ddd-spacing-4);
@@ -71,7 +66,7 @@ export class haxcms extends DDD {
                 margin: var(--ddd-spacing-3);
                 padding: var(--ddd-spacing-6);
                 width: 92%;
-                background-color: var(--ddd-theme-default-nittanyNavy);
+                background-color: var(--ddd-theme-default-Navy);
 
             }
 
@@ -98,7 +93,7 @@ export class haxcms extends DDD {
                 margin: var(--ddd-spacing-3);
                 padding: var(--ddd-spacing-6);
                 border: var(--ddd-border-sm);
-                border-color: var(--ddd-theme-default-nittanyNavy);
+                border-color: var(--ddd-theme-default-Navy);
                 background-color: var(--ddd-theme-default-roarMaxlight);
                 position: sticky;
                 left: 55%;
@@ -106,7 +101,7 @@ export class haxcms extends DDD {
         }
         .Invite:focus,
         .Invite:hover {
-            background-color: var(--ddd-theme-default-nittanyNavy);
+            background-color: var(--ddd-theme-default-Navy);
                 color: var(--ddd-theme-default-roarMaxlight);
                 transform: scale(1.1);
                 transition: 0.3s ease-in-out;
@@ -122,14 +117,14 @@ export class haxcms extends DDD {
             margin: var(--ddd-spacing-3);
             padding: var(--ddd-spacing-6);
             border: var(--ddd-border-sm);
-            border-color: var(--ddd-theme-default-nittanyNavy);
+            border-color: var(--ddd-theme-default-Navy);
             background-color: var(--ddd-theme-default-roarMaxlight);
             position: sticky;
             left: 90%;
         }
         .remove:focus,
         .remove:hover {
-            background-color: var(--ddd-theme-default-nittanyNavy);
+            background-color: var(--ddd-theme-default-Navy);
             color: var(--ddd-theme-default-roarMaxlight);
             transform: scale(1.1);
             transition: 0.3s ease-in-out;
@@ -145,25 +140,106 @@ export class haxcms extends DDD {
                 margin: var(--ddd-spacing-3);
                 padding: var(--ddd-spacing-6);
                 border: var(--ddd-border-sm);
-                border-color: var(--ddd-theme-default-nittanyNavy);
+                border-color: var(--ddd-theme-default-Navy);
                 background-color: var(--ddd-theme-default-roarMaxlight);
                 position: sticky;
                 left: 90%;
         }
         .Save:focus,
         .Save:hover {
-            background-color: var(--ddd-theme-default-nittanyNavy);
+            background-color: var(--ddd-theme-default-Navy);
             color: var(--ddd-theme-default-roarMaxlight);
             transform: scale(1.1);
             transition: 0.3s ease-in-out;
         }
-
-    
-        
-    
+        #search-input {
+                font-family: system-ui;
+                min-width: 43%;
+                margin: var(--ddd-spacing-3);
+                padding: var(--ddd-spacing-6);
+                background-color: var(--ddd-theme-default-slateMaxLight);
+            }
 
   `];
 }
+
+
+addItem() {
+    const entry = this.shadowRoot.getElementById("search-input");
+    const username = entry.value.trim();
+
+    if (username !== "") { 
+        if (this.party.length < 5) { 
+            if (/^[a-z0-9]{1,10}$/.test(username)) { 
+                if (!this.party.includes(username)) { 
+                    this.party = [...this.party, username];
+                    this.toggleChanged();
+                    console.log(this.party);
+
+                } else {
+                    alert("Username is already in the party.");
+                }
+            } else {
+                alert("Check Username rules.");
+            }
+        } else {
+            alert("The party has the maximum number of members.");
+        }
+    } else {
+        alert("Text imput is empty.");
+    }
+}
+toggleChanged() {
+    this.changed = !this.changed;
+}
+
+remove(e){
+    const id = e.target.id;
+    this.selectedUser = id;
+    this.delete = true;
+    const position = this.party.indexOf(this.selectedUser); 
+    let deleteRequest = "Are you sure you want to remove the member:" + this.selectedUser + "?"; 
+
+    if(confirm(deleteRequest) == true){ 
+        this.party.splice(position, 1);
+        this.selectedUser = '';
+        this.deleteUserPending = false;
+        console.log(localStorage.getItem("party").split(","));
+        this.toggleChanged();
+        this.requestUpdate();
+    } else {
+        alert("User removal canceled."); 
+        console.log(localStorage.getItem("party").split(","));
+        this.userToDelete = '';
+        this.selectedUser = false;
+        this.requestUpdate();
+    }
+    this.delete = false; 
+}
+
+save() {
+    if (this.changed) { 
+        alert("Party saved!");
+        const partyArray = this.party.toString();
+        localStorage.setItem("party", partyArray);
+        console.log(localStorage.getItem("party").split(","));
+        this.saved = true;
+        this.makeItRain();
+        this.toggleChanged();
+    } else {
+        alert("No changes were made to the party or the party is empty.");
+        this.toggleChanged();
+    }
+}
+
+displayItem(item) {  
+    if (this.saved) { 
+        return html`<div class="userSlot"><rpg-character walking seed=${item}></rpg-character><p class="username">${item}</p><button class="remove" id="${item}" @click="${this.remove}">Remove Member</button></div>`;
+    } else {
+        return html`<div class="userSlot"><rpg-character seed=${item}></rpg-character><p class="username">${item}</p><button class="remove" id="${item}" @click="${this.remove}">Remove Member</button></div>`;
+    }
+}
+
 makeItRain() {
     import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
         (module) => {
@@ -179,32 +255,55 @@ render() {
    
     
     return html`
-        <div>
-                <button class="Save">Save Party</button>
-                <button class="Invite"><img class=Icon src="${this.icon}" alt="Add Friend">Invite Friend</button>
-            </div>
+       <confetti-container id="confetti">
+  <div class="partyList">
+      <h2 class="title">Create Party</h2>
+      <p class="ruleText" style="font-family: comic sans ms; text-align: center;"></p>
+      <div class="rules">
+              <p class="ruleText">Input Rules:</p>
+              <p class="ruleText">- Maximum of 10 characters.</p>
+              <p class="ruleText">- Only lowercase letters and numbers.</p>
+              <p class="ruleText">- No special characters.</p>
+              <p class="ruleText">- The party can only have a maximum of 5 members.</p>
+              <p class="ruleText">- No duplicate members.</p>
+              <p></p>
+              <details><summary class="ruleText" style="color: white;">Current Array:</summary><div><p class="ruleText" style="color: white;">${this.party}</p></div></details>
+      </div>
+      
+      <div class="buttonWrapper">
+          <input id="search-input" type="text" placeholder="Add a party member."/>
+          <button id="Invite" @click="${this.addItem}">Invite Friend</button>
+          <button id="Save" @click="${this.saveData}">Save Party</button>
+          
+          <script>
+              var input = document.getElementById("search-input");
+              input.addEventListener("keypress", function(e) {
+              if (e.key === "enter") {
+                  document.getElementById("Invite").click();
+              }
+          });
+          </script>
 
-            <div class="List">
-                <div>
-                    <rpg-character hat="random" seed="haxtheweb"></rpg-character>
-                    <p style="text-aign: left; "> </p>
-                </div>
-                <button class="remove">Remove Member</button>
-            
-            </div>
-            
-            <p style="font-family: comic sans ms;"> </p>
-            
+      </div>
+
+      <div class="partyDisplay">
+          ${this.party.map((item) => this.displayItem(item))}           
+      </div>
+  
+  </div>
+  </confetti-container>               
     `;
   }
     
 
-
   static get properties() {
     return { 
-      icon: { type: String },
-      darkMode: { type: Boolean, reflect: true, attribute: "dark-mode"},
-
+        ...super.properties,
+        delete: { type: Boolean, reflect: true },
+        changed: { type: Boolean, reflect: true },
+        saved: { type: Boolean, relect: true },
+        party: { type: Array, reflect: true },
+        selectedUser: { type: String, reflect: true },  
       };
     }
 }
